1856910fc1ef3d8b013385a54325d3fa53d16059
Wescley <wescleygaldino06@gmail.com>
2024-10-21 08:28:23 -0300

    Update responsivity

--- ppi/views.py
+++ ppi/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "range" : range(3)
+        "range" : range(6)
     }
     return render(request, "index.html", context)
 
