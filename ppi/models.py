9bb84559cdad0822b3b14c5d2cc29f154ae0135f
Wescley <wescleygaldino06@gmail.com>
2024-12-01 16:52:20 -0300

    DB

--- ppi/models.py
+++ ppi/models.py
@@ -26,11 +26,11 @@ class Post(models.Model):
         return self.titulo
     
 class Curso(models.Model):
-    nome = models.CharField(max_length=50)
-    logo = models.ImageField()
+    titulo = models.CharField(max_length=50)
+    logo = models.ImageField(blank=True)
     
     def __str__(self):
-        return self.nome
+        return self.titulo
 
 class Projeto(models.Model):
     titulo = models.CharField(max_length=80)
@@ -38,6 +38,15 @@ class Projeto(models.Model):
     resumo = models.TextField(max_length=2000)
     capa = models.ImageField()
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    usuarios1 = models.ManyToManyField(Usuario1)
+    usuarios2 = models.ManyToManyField(Usuario2)
     
     def __str__(self):
-        return self.nome
+        return self.titulo
+    
+class FotoProjeto(models.Model):
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, related_name='photos')
+    photo = models.ImageField()
+
+    def __str__(self):
+        return f"Foto do projeto {self.projeto.titulo}"
