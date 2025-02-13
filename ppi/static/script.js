25ed055fe4cc7691de42f68424a5521d988940cb
wescley <wescleygaldino06@gmail.com>
2025-02-13 15:55:52 -0300

    Configurando navbar (habilitando offcanvas)

--- ppi/static/script.js
+++ ppi/static/script.js
@@ -1,17 +1,3 @@
-
-const pesquisarBtn = document.getElementById('pesquisar');
-
-if (pesquisarBtn) {
-    pesquisarBtn.addEventListener('click', () => {
-        event.stopPropagation();
-        pesquisarBtn.classList.toggle('mudarTamanho')
-    });
-    
-    document.addEventListener('click', function() {
-        pesquisarBtn.classList.remove('mudarTamanho');
-    });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const deleteBtns = document.querySelectorAll('.delete-btn')
 
